65983034667f387c2a72b314082077145664fe42
Berke Özgen <berkeozgen08@hotmail.com>
2020-12-01 16:55:41 +0300

    Added scaling

--- ImagePanel.java
+++ ImagePanel.java
@@ -4,6 +4,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -24,6 +26,19 @@ public class ImagePanel extends JPanel {
 	public void setImage(File file) {
 		try {
 			image = ImageIO.read(file);
+			int width = image.getWidth();
+			int height = image.getHeight();
+			if (width >= 800 || height >= 600) {
+				double scale = width >= 800 ? 800.0 / width : 600.0 / height;
+				int scaledWidth = (int) (width * scale);
+				int scaledHeight = (int) (height * scale);
+				BufferedImage scaledImage = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D graphics2D = scaledImage.createGraphics();
+				graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+				graphics2D.drawImage(image, 0, 0, scaledWidth, scaledHeight, null);
+				graphics2D.dispose();
+				image = scaledImage;
+			}
 			imageLabel.setIcon(new ImageIcon(image));
 			frame.pack();
 		} catch (Exception e) {
